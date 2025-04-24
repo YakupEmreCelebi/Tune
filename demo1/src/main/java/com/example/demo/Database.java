48b0beeba00457e375089d1787ce100719cc5686
kaptankaanyildirim <kaptankaanyildirim0@gmail.com>
2025-04-24 20:26:51 +0300

    database

---
+++ demo1/src/main/java/com/example/demo/Database.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.example.User;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    public ArrayList<User> getAllFriendsFromDatabase(String username) {
+
+        return new ArrayList<>();
+    }
+
+    public void removeFriendFromDatabase(String username, String friendUsername) {
+
+    }
+
+    public void addFriendToDatabase(String username, String friendUsername) {
+
+    }
+
+    public void addUserToDatabase(String username, String email, String password) {
+
+    }
+
+    public void removeUserFromDatabase(String username) {
+
+    }
+
+    public void updateUserInDatabase(String username, String email, String password) {
+
+    }
+
+    public User getUserFromDatabase(String username) {
+        return new User();
+    }
+
+    public ArrayList<Song> getFavoritesFromDatabase(String username) {
+        return new ArrayList<>();
+    }
+
+    public void addSongToFavorites(String username, String songName) {
+
+    }
+
+    public void removeSongFromFavorites(String username, String songName) {
+
+    }
+
+    public void controlUserInDatabase(String username, String password) {
+
+    }
+
+
+
+
+
+}
