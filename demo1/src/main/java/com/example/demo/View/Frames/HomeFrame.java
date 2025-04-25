1e9c81519891558dfb6b49f385713294df70f826
emre <ykpmrclb06@gmail.com>
2025-04-25 21:09:29 +0300

    PopUpShowFriendTune added. Not done

--- demo1/src/main/java/com/example/demo/View/Frames/HomeFrame.java
+++ demo1/src/main/java/com/example/demo/View/Frames/HomeFrame.java
@@ -1,30 +1,45 @@
 package com.example.demo.View.Frames;
 
 import com.example.demo.View.SpecialNodes.NavigateBar;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class HomeFrame extends Scene {
 
-    private Label label;
     private NavigateBar navigateBar;
-
-
+    private VBox container;
+    private TextField searchBar;
 
     public HomeFrame() {
-        super(new VBox(), getScreenWidth(), getScreenHeight());
+        super(new HBox(40), getScreenWidth(), getScreenHeight());
 
         this.getStylesheets().add(getClass().getResource("navBar.css").toExternalForm());
+        container = new VBox();
+        container.setPadding(new Insets(100,0,0,0));
+        container.setAlignment(Pos.TOP_CENTER);
+        container.setStyle("-fx-border-color: black");
+        container.setPrefWidth(getScreenWidth() - getScreenWidth() / 5.5 - 80);
 
-        Label label = new Label("HOME PAGE");
+        searchBar = new TextField();
+        searchBar.setPromptText("Search Music");
+        searchBar.setStyle("-fx-border-radius: 30px; -fx-background-radius: 30px; -fx-focus-color: transparent; -fx-faint-focus-color: transparent; -fx-border-color: #ccc;");
+        searchBar.setMaxWidth(350);
+        searchBar.setPrefHeight(40);
 
-        VBox layout = (VBox) getRoot();
-        layout.setSpacing(40);
+        container.getChildren().add(searchBar);
+
+        HBox layout = (HBox) getRoot();
+        layout.setOnMousePressed(new ChangeFocus());
 
         navigateBar = new NavigateBar();
 
-        layout.getChildren().addAll(navigateBar, label);
+        layout.getChildren().addAll(navigateBar, container);
     }
 
     private static double getScreenWidth() {
@@ -37,6 +52,18 @@ public class HomeFrame extends Scene {
 
     public NavigateBar getNavigateBar() {
         return navigateBar;
+    }
+
+    private class ChangeFocus implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            if(searchBar.isFocused())
+            {
+                getRoot().requestFocus();
+                searchBar.clear();
+            }
+        }
     }
 }
 
