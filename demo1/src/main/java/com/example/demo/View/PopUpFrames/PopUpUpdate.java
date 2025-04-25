3eb9e04d8c407ebacabd400658bd106869653847
emre <ykpmrclb06@gmail.com>
2025-04-26 00:08:21 +0300

    PopUpUpdate done

--- demo1/src/main/java/com/example/demo/View/PopUpFrames/PopUpUpdate.java
+++ demo1/src/main/java/com/example/demo/View/PopUpFrames/PopUpUpdate.java
@@ -5,6 +5,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -17,10 +18,28 @@ public class PopUpUpdate extends PopUp{
     public PopUpUpdate(String title, String textAreaPrompt, String buttonText) {
         super();
 
+        // Label
         label = new Label(title);
+        label.setStyle("-fx-font-size: 17");
+
+        HBox labelContainer = new HBox(label);
+        labelContainer.setMaxWidth(260);
+        labelContainer.setAlignment(Pos.CENTER);
+        labelContainer.setAlignment(Pos.CENTER_LEFT);
+
+        //Text Field
         textField = new TextField();
         textField.setPromptText(textAreaPrompt);
+        textField.setMaxWidth(260);
+        textField.setPrefHeight(30);
+        textField.setStyle("-fx-background-color: transparent; -fx-border-color: #8e8989; -fx-border-width: 1px; -fx-border-radius: 5px;");
+
+        //Button
         updateButton = new Button(buttonText);
+        updateButton.setPrefWidth(260);
+        updateButton.setPrefHeight(35);
+        updateButton.setStyle("-fx-border-radius: 10; -fx-background-radius: 10; -fx-background-color: #171616; -fx-text-fill: white; -fx-font-size: 15");
+
 
         VBox layout = (VBox) getRoot();
 
@@ -28,7 +47,7 @@ public class PopUpUpdate extends PopUp{
         layout.setSpacing(15);
         layout.setPadding(new Insets(20));
 
-        layout.getChildren().addAll(label, textField, updateButton);
+        layout.getChildren().addAll(labelContainer, textField, updateButton);
 
         // Close when button pressed
         updateButton.setOnAction(e -> {
